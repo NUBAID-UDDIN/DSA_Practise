363f531bc3a3e55165cdb9da744b7e6d8f988ac1
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-08-23 15:49:56 +0530

    Refactor deleteNode method to simplify deletion
    Simplified the deleteNode method by directly updating the node's value and next pointer.

--- JAVA_SOLUTION/237. Delete Node in a Linked List.java
+++ JAVA_SOLUTION/237. Delete Node in a Linked List.java
@@ -1,13 +1,6 @@
 class Solution {
     public void deleteNode(ListNode node) {
-        ListNode prev = null;
-        
-        while (node.next != null) {
-            node.val = node.next.val;
-            prev = node;
-            node = node.next;
-        }
-        
-        prev.next = null;
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
