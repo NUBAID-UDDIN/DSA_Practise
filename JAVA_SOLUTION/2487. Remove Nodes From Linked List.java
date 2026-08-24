4a6e684799da2f5e17a28a52c6ba4006b5479df2
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-08-24 12:36:41 +0530

    Implement removeNodes method to filter linked list

--- JAVA_SOLUTION/2487. Remove Nodes From Linked List.java
+++ JAVA_SOLUTION/2487. Remove Nodes From Linked List.java
@@ -1,1 +1,34 @@
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        head = reverse(head);
 
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        ListNode curr = head;
+        int maxVal = 0;
+
+        while (curr != null) {
+            if (curr.val >= maxVal) {
+                maxVal = curr.val;
+                tail.next = curr;
+                tail = curr;
+            }
+            curr = curr.next;
+        }
+        tail.next = null;
+
+        return reverse(dummy.next);
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
