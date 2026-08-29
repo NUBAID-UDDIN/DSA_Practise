7944d0a2c89e16803e26b0a530cf31e089c6eb52
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-08-30 00:36:03 +0530

    Implement solution for Poor Pigs problem

--- JAVA_SOLUTION/458. Poor Pigs.java
+++ JAVA_SOLUTION/458. Poor Pigs.java
@@ -1,1 +1,14 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int statesPerPig = minutesToTest / minutesToDie + 1;
+        int pigs = 0;
+        int totalStates = 1;
 
+        while (totalStates < buckets) {
+            pigs++;
+            totalStates *= statesPerPig;
+        }
+
+        return pigs;
+    }
+}
