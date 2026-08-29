3822ec93ca22128925012a605ae59961dac28fab
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-08-30 00:38:19 +0530

    Implement GCD pair queries solution in Java

--- JAVA_SOLUTION/3312. Sorted GCD Pair Queries.java
+++ JAVA_SOLUTION/3312. Sorted GCD Pair Queries.java
@@ -1,1 +1,56 @@
+import java.util.Arrays;
 
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int max = 0;
+        for (int x : nums) {
+            max = Math.max(max, x);
+        }
+
+        int[] freq = new int[max + 1];
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+        long[] mult = new long[max + 1];
+        for (int i = 1; i <= max; i++) {
+            for (int j = i; j <= max; j += i) {
+                mult[i] += freq[j];
+            }
+        }
+
+        long[] gcd = new long[max + 1];
+        for (int i = max; i >= 1; i--) {
+            long pairs = mult[i] * (mult[i] - 1) / 2;
+            for (int j = 2 * i; j <= max; j += i) {
+                pairs -= gcd[j];
+            }
+            gcd[i] = pairs;
+        }
+
+        long[] pref = new long[max + 1];
+        for (int i = 1; i <= max; i++) {
+            pref[i] = pref[i - 1] + gcd[i];
+        }
+
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = search(pref, queries[i]);
+        }
+
+        return ans;
+    }
+
+    private int search(long[] pref, long target) {
+        int L = 0, R = pref.length;
+        while (L < R) {
+            int mid = L + (R - L) / 2;
+            if (pref[mid] <= target) {
+                L = mid + 1;
+            } else {
+                R = mid;
+            }
+        }
+        return L;
+    }
+}
