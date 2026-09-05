134281c044db417845d0c82c5656435bcfe9344b
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-09-06 00:37:06 +0530

    Implement insertion sort for a linked list

---
+++ JAVA_SOLUTION/147. Insertion Sort List.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        List<Integer> values = new ArrayList<>();
+        ListNode curr = head;
+
+        while (curr != null) {
+            values.add(curr.val);
+            curr = curr.next;
+        }
+
+        Collections.sort(values);
+
+        curr = head;
+        for (int val : values) {
+            curr.val = val;
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
