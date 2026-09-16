2d7492eee1a7adc3d77e3a24deff0c329e12f13c
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-09-16 19:24:38 +0530

    Implement minMoves2 to calculate minimum moves

---
+++ JAVA_SOLUTION/462. Minimum Moves to Equal Array Elements II.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        
+        int left = 0;
+        int right = nums.length - 1;
+        int moves = 0;
+        
+        while (left < right) {
+            moves += nums[right] - nums[left];
+            left++;
+            right--;
+        }
+        
+        return moves;
+    }
+}
