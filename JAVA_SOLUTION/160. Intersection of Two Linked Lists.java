a9d6a4b40c1908b2484eb4b0f786b78711921535
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-09-20 12:54:01 +0530

    Simplify pointer traversal in getIntersectionNode
    Refactor intersection detection logic for clarity.

--- JAVA_SOLUTION/160. Intersection of Two Linked Lists.java
+++ JAVA_SOLUTION/160. Intersection of Two Linked Lists.java
@@ -1,14 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
@@ -17,22 +6,12 @@ public class Solution {
         
         ListNode ptrA = headA;
         ListNode ptrB = headB;
-        
-        // Loop until both pointers meet (at the intersection node or at null)
+      
         while (ptrA != ptrB) {
-     
-            if (ptrA == null) {
-                ptrA = headB;
-            } else {
-                ptrA = ptrA.next;
-            }
-            
-            if (ptrB == null) {
-                ptrB = headA;
-            } else {
-                ptrB = ptrB.next;
-            }
+            ptrA = (ptrA == null) ? headB : ptrA.next;
+            ptrB = (ptrB == null) ? headA : ptrB.next;
         }
+   
         return ptrA;
     }
 }
