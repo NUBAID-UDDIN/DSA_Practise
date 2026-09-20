3a9137d9b6ccc1b2fc0071c18d1abcae3dd4744a
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-09-20 12:36:33 +0530

    Change return type of reverseDegree to int

--- JAVA_SOLUTION/3498. Reverse Degree of a String.java
+++ JAVA_SOLUTION/3498. Reverse Degree of a String.java
@@ -1,14 +1,15 @@
-public class Solution {
-    public long reverseDegree(String s) {
+class Solution {
+    public int reverseDegree(String s) {
         long totalDegree = 0;
         int n = s.length();
         
         for (int i = 0; i < n; i++) {
             int reverseRank = 26 - (s.charAt(i) - 'a');
-            
+  
             totalDegree += (long) reverseRank * (i + 1);
         }
         
-        return totalDegree;
+        return (int) totalDegree;
+        
     }
 }
