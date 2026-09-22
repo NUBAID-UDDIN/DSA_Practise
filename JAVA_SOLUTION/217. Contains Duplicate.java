18283c893d0c6d045d7ee6d39db7092092e04085
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-09-22 22:57:32 +0530

    Optimize containsDuplicate method using HashSet
    Refactored containsDuplicate method to use a HashSet for improved efficiency.

--- JAVA_SOLUTION/217. Contains Duplicate.java
+++ JAVA_SOLUTION/217. Contains Duplicate.java
@@ -1,13 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+        Set<Integer> seen = new HashSet<>();
+        
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
             }
+            seen.add(num);
         }
+        
         return false;
     }
 }
