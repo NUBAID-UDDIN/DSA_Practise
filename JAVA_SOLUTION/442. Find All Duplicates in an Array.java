b702120ee528b3f29ccf5bd4f51b9f6bb3f976ee
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-09-26 00:04:08 +0530

    Add solution to find all duplicates in an array

---
+++ JAVA_SOLUTION/442. Find All Duplicates in an Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> duplicates = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                duplicates.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+
+        return duplicates;
+    }
+}
