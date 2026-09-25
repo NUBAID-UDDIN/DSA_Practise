f32edd688fd2d4f4bce14b063690e1c493661b06
NUBAID UDDIN <nubaid.uddin2007@gmail.com>
2026-09-26 00:09:51 +0530

    Implement solution to find disappeared numbers in array

---
+++ JAVA_SOLUTION/448. Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            seen.add(num);
+        }
+
+        List<Integer> missing = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!seen.contains(i)) {
+                missing.add(i);
+            }
+        }
+
+        return missing;
+    }
+}
